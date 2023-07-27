d691177a612fbac20c4eea36e6cf50c3fffbe3a2
Dawson Schroeder <124729223+Dawson-Schroeder@users.noreply.github.com>
2023-07-27 13:51:06 -0500

    first commit and setup

---
+++ src/main/java/com/example/filesafe/FileSafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filesafe/FileSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
